84067b566089c6ee6948f26f19bd42aeb196d082
Toheeb <adebusoyeteeman@gmail.com>
2024-04-22 10:03:20 +0100

    expenseTracker

---
+++ expense-tracker/src/components/IncomeExpenses.jsx
@@ -0,0 +1,66 @@
+import  { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+//Money formatter function
+function moneyFormatter(num) {
+  let p = num.toFixed(2).split('.');
+  return (
+    '$ ' +
+    p[0]
+      .split('')
+      .reverse()
+      .reduce(function (acc, num, i, orig) {
+        return num === '-' ? acc : num + (i && !(i % 3) ? ',' : '') + acc;
+      }, '') +
+    '.' +
+    p[1]
+  );
+}
+
+export const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
+    -1
+  );
+  
+// function Balance(){
+ 
+// export const Balance = () => {
+//   const { transactions } = useContext(GlobalContext);
+
+//   const amounts = transactions.map(transaction => transaction.amount);
+
+//   const total = amounts.reduce((acc, item) => (acc += item), 0);
+   
+//   return (
+//     <>
+//       <h4>Your Balance</h4>
+//     <h1>{moneyFormatter(total)}</h1>
+//     <h1>${total}</h1>
+//       </>
+//   )
+// }
+// export default Balance
+
+
+  return (
+    <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+  <p className="money plus">{moneyFormatter(income)}</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+  <p className="money minus">{moneyFormatter(expense)}</p>
+        </div>
+      </div>
+  )
+}
